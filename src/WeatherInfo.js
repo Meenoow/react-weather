802b71e23c4e3da75bc221caadc1066504789c58
Meenoow <100647073+Meenoow@users.noreply.github.com>
2023-09-13 14:39:28 -0500

    included animated icons and a new component to include in main page for rendering

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
     return (
@@ -13,12 +14,12 @@ export default function WeatherInfo(props) {
             <div className="row mt-3">
                 <div className="col-6">
                     <div className="clearfix">
-                        <img 
-                        src={props.data.iconUrl}
-                        alt={props.data.description}
-                        className="float-left" 
-                        />
-                        <WeatherTemperature celsius={props.data.temperature}/>
+                        <div className="float-left">
+                            <WeatherIcon code={props.data.icon}/>
+                        </div>
+                        <div className="float-left">
+                         <WeatherTemperature celsius={props.data.temperature}/>
+                        </div>
                     </div>    
                 </div>
                 <div className="col-6">
