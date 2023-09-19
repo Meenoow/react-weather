b2a93625aee527efe1ab4d09f1a7cbd55e113408
Meenoow <100647073+Meenoow@users.noreply.github.com>
2023-09-19 11:15:49 -0500

    refactored code and fixed warnings

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Weather defaultCity="Berlin" />
+      <Weather defaultCity="New York" />
       <footer>
-        Coded by <a href="">Ximena Castillo</a> and{" "}
-        <a href="https://github.com/Meenoow/react-weather" target="_blank">
+        Coded by 
+        <a 
+        href=""
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        Ximena Castillo
+        </a> and{" "}
+        <a 
+        href="https://github.com/Meenoow/react-weather" 
+        target="_blank"
+        rel="noopener noreferrer"
+        >
         Open-sourced on GitHub
         </a> 
       </footer>  
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,15 +1,29 @@
-import React , { useState } from "react";
+import React , { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
+import { cleanup } from "@testing-library/react";
 
 export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
 
+    useEffect(() => {
+       setLoaded(false); 
+    }, [props.coordinates]);
+
     function handleResponse(response){
         setForecast(response.data.daily);
         setLoaded(true);
+    }
+
+    function load(){
+        let apiKey = "0d55405ea37f9b16a55f03b2fb1326a2";
+        let longitude = props.coordinates.lon;
+        let latitude = props.coordinates.lat;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}&unit=metric`;
+    
+        axios.get(apiUrl).then(handleResponse);
     }
 
     if (loaded) {
@@ -23,17 +37,16 @@ export default function WeatherForecast(props) {
                                     <WeatherForecastDay data={dailyForecast} />
                                 </div>
                             );
+                        } else {
+                            return null;
                         }
                     })}
                 </div>
             </div>
         );
     } else {
-        let apiKey = "0d55405ea37f9b16a55f03b2fb1326a2";
-        let longitude = props.coordinates.lon;
-        let latitude = props.coordinates.lat;
-        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}&unit=metric`;
-    
-        axios.get(apiUrl).then(handleResponse);
+        load();
+
+        return null;
     }
 }
